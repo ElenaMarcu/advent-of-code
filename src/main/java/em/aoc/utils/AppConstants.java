a6e240e334e2d193a0c4e3577e7b73b62967ceef
Elena Marcu <elenamarcu292@gmail.com>
2023-11-28 19:25:02 +0200

    Reformatted AppConstants

--- src/main/java/em/aoc/utils/AppConstants.java
+++ src/main/java/em/aoc/utils/AppConstants.java
@@ -6,6 +6,32 @@ public class AppConstants {
   public static final String RESOURCES_PATH_MAIN = "src/main/resources/input/";
   public static final String YEAR_2023 = "year2023/";
   public static final String DAY1 = "Day1";
+  public static final String DAY2 = "Day2";
+  public static final String DAY3 = "Day3";
+  public static final String DAY4 = "Day4";
+  public static final String DAY5 = "Day5";
+  public static final String DAY6= "Day6";
+  public static final String DAY7 = "Day7";
+  public static final String DAY8 = "Day8";
+  public static final String DAY9 = "Day9";
+  public static final String DAY10 = "Day10";
+  public static final String DAY11 = "Day11";
+  public static final String DAY12 = "Day12";
+  public static final String DAY13 = "Day13";
+  public static final String DAY14 = "Day14";
+  public static final String DAY15 = "Day15";
+  public static final String DAY16= "Day16";
+  public static final String DAY17 = "Day17";
+  public static final String DAY18 = "Day18";
+  public static final String DAY19 = "Day19";
+  public static final String DAY20 = "Day20";
+  public static final String DAY21 = "Day21";
+  public static final String DAY22 = "Day22";
+  public static final String DAY23 = "Day23";
+  public static final String DAY24 = "Day24";
+  public static final String DAY25 = "Day25";
+
+
   public static final String TXT_EXTENSION = ".txt";
   private AppConstants() {
     throw new IllegalStateException("Utility class");
